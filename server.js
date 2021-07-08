017bdb9a9766a407de9a6f7b0777f39c15107295
Allan Demarbre <ademarbre@groupe-cac.com>
2021-07-08 16:15:06 +0200

    Adding a connection to the server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
+const http = require('http');
+const server = http.createServer(app);
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-})
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', socket => {
+    console.log('a user logged in');
+});
+
+server.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
